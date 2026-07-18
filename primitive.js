90f92ce3edf889622fd91e5019a43680e245e099
ranjithranjigit <ranjithgv.g@gmail.com>
2026-07-18 22:08:16 +0530

    Add files via upload
    Assignment1

---
+++ primitive.js
@@ -0,0 +1,11 @@
+let firstName = 'Ranjith'
+let companyName ='CTS'
+let mobileNumber = 963963
+let isAutomation = false
+let hasPlaywright 
+console.log (firstName)
+console.log (companyName)
+console.log (mobileNumber)
+console.log (isAutomation)
+console.log (hasPlaywright)
+console.log (typeof firstName,typeof companyName,typeof mobileNumber,typeof isAutomation,typeof hasPlaywright)
